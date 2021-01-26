ebaa0db4ef709b01594fe985e70f931651c56858
SupunRandunu <supun.randunu@gmail.com>
2021-01-26 09:40:45 +0530

    Update assets

---
+++ Assets/NewGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewGame : MonoBehaviour {
+	public Transform canvas;
+	// Use this for initialization
+	public GameObject Panel;
+	// Use this for initialization
+	public void newGame(){
+		//Application.LoadLevel (x);
+		//Panel.gameObject.SetActive(false);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		Time.timeScale = 1f;
+			canvas.gameObject.SetActive (false);
+
+	}
+}
---
+++ Assets/SetAndGetText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class SetAndGetText : MonoBehaviour {
+	public GameObject Panel;
+	// Use this for initialization
+	public void newGame(string x){
+		Application.LoadLevel (x);
+		//Panel.gameObject.SetActive(false);
+		//SceneManager.LoadScene(x);
+	
+	}
+
+
+}
